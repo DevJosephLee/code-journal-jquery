d40f1985a6248fb0eaf888c86b442db24a651a0c
Joseph Lee <teddylou0215@gmail.com>
2022-04-03 23:32:59 -0700

    configure toggle switch

--- js/main.js
+++ js/main.js
@@ -1,2 +1,22 @@
 /* global data */
 /* exported data */
+// $(".button").click(function(){
+//   $(".hello-world").text("Thanks for Clicking")
+// })
+let toggleSwitchClicked = false;
+
+$(".toggler-container").click(function() {
+  if (!toggleSwitchClicked) {
+    $(".toggler-black").attr("class", "toggler-white");
+    $(".toggler-container-black").attr("class", "toggler-container-white");
+    $(".toggle-text-black").attr("class", "toggle-text-white");
+    $(".bg-white").attr("class", "bg-black");
+    toggleSwitchClicked = true;
+  } else {
+    $(".toggler-white").attr("class", "toggler-black");
+    $(".toggler-container-white").attr("class", "toggler-container-black");
+    $(".toggle-text-white").attr("class", "toggle-text-black");
+    $(".bg-black").attr("class", "bg-white");
+    toggleSwitchClicked = false;
+  }
+})
